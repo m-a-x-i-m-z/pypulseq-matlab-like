d2897214f168f4a38bd13f9ba57bb37cc26befff
Maxim Zaitsev <maxim.zaitsev@uniklinik-freiburg.de>
2026-07-30 09:33:30 +0200

    adding an initial recon example script, which is based on the recon routines of Frank Zijlstra he has developed for the MR Physics with Pulseq course

---
+++ examples/demoRecon/recon_example.py
@@ -0,0 +1,43 @@
+# this script is based on the recon code / notebooks from the course "MR Physics with Pulseq" 
+import numpy as np
+import twixtools
+
+import math
+import matplotlib.pyplot as plt
+import pypulseq as pp
+#import mapvbvd
+from recon_utils import reconstruct, read_raw_data, read_siemens_raw_data, plot_nd
+import glob
+import os
+
+# directory to be scanned for data and sequence files
+data_path = '/home/zaitsev/range_software/pulseq/IceNIH_RawSend/'; 
+#data_path='/dev/shm/mr0mat/';
+#data_path='/dev/shm/koma_mat/';
+
+files = glob.glob(os.path.join(data_path, "*.seq"))
+files.sort(key=os.path.getmtime)
+
+seq_file_path=files[-1]  # take the last modified sequence file
+
+basic_filename = os.path.splitext(seq_file_path)[0]  # remove the extension
+
+data_file_path=basic_filename + '.dat'  # try Siemens TWIX file
+
+print(f'Loading raw data file \'{data_file_path}\'')
+data_unsorted = read_siemens_raw_data(data_file_path)  # 3D numpy array [n_column, n_channel, acquisition_counter]
+
+# load the sequence file
+print(f'Loading sequence file \'{seq_file_path}\'')
+seq = pp.Sequence()
+seq.read(seq_file_path, detect_rf_use=True) 
+
+seq_name_definition = seq.get_definition('Name');
+if seq_name_definition is not None:
+    print(f'Sequence name: {seq_name_definition}')
+else:
+    print('Sequence name is not defined in the sequence file.')
+
+rec = reconstruct(data_unsorted, seq)
+
+plot_nd(rec)
